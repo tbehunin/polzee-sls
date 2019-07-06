a3618edb3e55aea971ff7de2139e4eccb45df417
todd.behunin@nike.com <todd.behunin@nike.com>
2019-07-06 09:43:04 -0700

    generateToken stub - with cors support

--- handler.js
+++ handler.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const dynamodb = require('serverless-dynamodb-client');
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+};
 
 module.exports.hello = async (event, context) => {
     const params = {
@@ -28,4 +32,16 @@ module.exports.hello = async (event, context) => {
 
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
     // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.generateToken = async (event, context) => {
+    const response = {
+        statusCode: 200,
+        headers: { ...corsHeaders },
+        body: JSON.stringify({
+            todd: 'was here',
+            input: event,
+        }),
+    };
+    return response;
 };
