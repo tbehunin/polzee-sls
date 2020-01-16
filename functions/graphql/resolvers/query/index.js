fdce9d2f92f3f0840862000d1c5f83327e925568
Todd Behunin <tbehunin@cvent.com>
2020-01-16 12:08:34 -0800

    removed pollResolver in favor of pollTypeResolver ftw!

--- functions/graphql/resolvers/query/index.js
+++ functions/graphql/resolvers/query/index.js
@@ -1,11 +1,10 @@
 import polls from './pollsResolver';
-import poll from './pollResolver';
 import directPolls from './directPollsResolver';
 import withPollAuthorization from './withPollAuthorization';
 
 export default {
   polls,
-  poll: withPollAuthorization(poll),
+  poll: withPollAuthorization((_, { pollId }) => ({ pollId })),
   directPolls,
   user: (_, { userId }) => ({ userId }),
   // feed: (source, args, context, state) => data.polls,
--- functions/graphql/resolvers/types/pollTypeResolver.js
+++ functions/graphql/resolvers/types/pollTypeResolver.js
@@ -1,3 +1,40 @@
+import { ApolloError } from 'apollo-server-lambda';
+
+const pollService = {
+  getPoll: async (pollId, pollLoader) => {
+    let poll;
+    try {
+      poll = await pollLoader.load(pollId);
+    } catch (error) {
+      console.error(error);
+      throw new ApolloError(`Error getting poll ${pollId}`);
+    }
+    if (!poll) {
+      throw new ApolloError(`Poll ${pollId} not found`);
+    }
+    return poll;
+  },
+};
+
+const pollProps = ['pollId', 'createTimestamp', 'question', 'sharedWith', 'expireTimestamp']
+  .reduce((acc, prop) => {
+    acc[prop] = async ({ pollId }, _, { loaders }) => {
+      const poll = await pollService.getPoll(pollId, loaders.poll);
+      return poll[prop];
+    };
+    return acc;
+  }, {});
+
 export default {
-  choices: async ({ choices, ...poll }) => (choices || []).map((choice) => ({ poll, ...choice })),
+  ...pollProps,
+  choices: async ({ pollId }, _, { loaders }) => {
+    // Some choice props need info from the poll in order to validate and resolve properly
+    // so send the poll data along with EACH choice.
+    const poll = await pollService.getPoll(pollId, loaders.poll);
+    return (poll.choices || []).map((choice) => ({ poll, ...choice }));
+  },
+  user: async ({ pollId }, _, { loaders }) => {
+    const poll = await pollService.getPoll(pollId, loaders.poll);
+    return { userId: poll.userId };
+  },
 };
