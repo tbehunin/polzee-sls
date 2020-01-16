47a433e66d0417da9f49f2e79043462fc0ed6bb5
Todd Behunin <tbehunin@cvent.com>
2020-01-16 10:51:00 -0800

    moved file location

--- functions/graphql/resolvers/index.js
+++ functions/graphql/resolvers/index.js
@@ -1,8 +1,8 @@
 import GraphQLBigInt from 'apollo-type-bigint';
 import Query from './query';
 import Mutation from './mutation';
-import pollTypeResolver from './pollTypeResolver';
-import pollChoiceTypeResolver from './pollChoiceTypeResolver';
+import pollTypeResolver from './types/pollTypeResolver';
+import pollChoiceTypeResolver from './types/pollChoiceTypeResolver';
 import userTypeResolver from './types/userTypeResolver';
 
 export default {
