c06c439560c0ea6af03b2921b143feaed4d8d4d1
Todd Behunin <tbehunin@cvent.com>
2020-03-15 23:47:42 -0700

    refactor to use getItem with authorization

--- functions/graphql/resolvers/types/pollTypeResolver.js
+++ functions/graphql/resolvers/types/pollTypeResolver.js
@@ -1,34 +1,19 @@
-import { ApolloError, ForbiddenError } from 'apollo-server-lambda';
+import services from '../../../../services';
 import query from '../../../../data/polls/query';
 
-const pollService = {
-  getPoll: async (pollId, currentUserId, pollLoader) => {
-    let poll;
-    try {
-      poll = await pollLoader.load(pollId);
-    } catch (error) {
-      console.error(error);
-      throw new ApolloError(`Error getting poll ${pollId}`);
-    }
-    if (!poll) {
-      throw new ApolloError(`Poll ${pollId} not found`);
-    }
-
-    // Validate user has access to view this poll
-    if (poll.userId !== currentUserId
-      && poll.sharedWith && poll.sharedWith.length > 0
-      && !poll.sharedWith.includes(currentUserId)) {
-      throw new ForbiddenError('Unauthorized');
-    }
-
-    return poll;
-  },
+const options = {
+  itemType: 'Poll',
+  throwWhenNotFound: true,
+  authorizerFn: (poll, currentUserId) => (
+    poll.userId === currentUserId || (poll.sharedWith || []).includes(currentUserId)
+  ),
 };
 
 const basicPropResolvers = ['pollId', 'createTimestamp', 'question', 'sharedWith', 'expireTimestamp']
   .reduce((acc, prop) => {
     acc[prop] = async ({ pollId }, _, { currentUserId, loaders }) => {
-      const poll = await pollService.getPoll(pollId, currentUserId, loaders.poll);
+      const svc = services(currentUserId, loaders.poll);
+      const poll = await svc.getItemByKey(pollId, options);
       return poll[prop];
     };
     return acc;
@@ -37,7 +22,8 @@ const basicPropResolvers = ['pollId', 'createTimestamp', 'question', 'sharedWith
 const toManyPropResolvers = ['comments', 'likes', 'votes']
   .reduce((acc, prop) => {
     acc[prop] = async ({ pollId }, _, { currentUserId, loaders }) => {
-      const poll = await pollService.getPoll(pollId, currentUserId, loaders.poll);
+      const svc = services(currentUserId, loaders.poll);
+      const poll = await svc.getItemByKey(pollId, options);
       return query[prop](poll.pollId);
     };
     return acc;
@@ -49,11 +35,13 @@ export default {
   choices: async ({ pollId }, _, { currentUserId, loaders }) => {
     // Some choice props need info from the poll in order to validate and resolve properly
     // so send the poll data along with EACH choice.
-    const poll = await pollService.getPoll(pollId, currentUserId, loaders.poll);
+    const svc = services(currentUserId, loaders.poll);
+    const poll = await svc.getItemByKey(pollId, options);
     return (poll.choices || []).map((choice) => ({ poll, ...choice }));
   },
   user: async ({ pollId }, _, { currentUserId, loaders }) => {
-    const poll = await pollService.getPoll(pollId, currentUserId, loaders.poll);
+    const svc = services(currentUserId, loaders.poll);
+    const poll = await svc.getItemByKey(pollId, options);
     return { userId: poll.userId };
   },
 };
